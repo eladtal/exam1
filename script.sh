3832c36847e1546a6aea5e0d9e942e761db233b4
Ubuntu <eladtalbi@DevOps-Ubu-TMP.1m304lza5ycelhl3bprlcm4esd.fx.internal.cloudapp.net>
2018-04-10 12:01:34 +0000

    final script

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo elad
+
+# $1 is the JOB NAME from Jenkins
+# $2 is the JOB NUMBER from Jenkins - which used to count every third attempt
+
+if [[ $(( $2%3)) == 0 ]] ; then
+    echo The running job is: $1;
+fi;
